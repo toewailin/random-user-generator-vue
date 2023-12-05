08b7d33bcffc6e5bd271fc30a7eacf9ecffafd76
toewailin <toewailinbdl@gmail.com>
2023-12-05 15:01:16 +0800

    fist comit

---
+++ app.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+    // template: '<h1>Hello Vue from {{firstName}}</h1>',
+    data() {
+        return {
+            firstName : "John",
+            lastName : "Nathan",
+            email: "john@email.com",
+            gender: "male",
+            picture: "https://randomuser.me/api/portraits/men/10.jpg"
+        }
+    },
+    methods: {
+        async getRandomUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const {results} = await res.json()
+            console.log("result", results);
+            this.firstName  =  results[0].name.first
+            this.lastName  =  results[0].name.last,
+            this.email =  results[0].email
+            this.gender =  results[0].gender,
+            this.picture =  results[0].picture.large
+        }
+    }
+})
+
+app.mount('#app')
